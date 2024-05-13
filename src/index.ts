955fe4de7f4af1727c1e35042e2828431376c6af
prasanth7890 <maryratnamp@gmail.com>
2024-05-13 22:46:46 +0530

    todos

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,11 @@ app.get('/connect', (req, res)=>{
 });
 
 // TODO:
-1. newbox - when a new box is creating...if its done, navigates to coding page else shows loading... on screen
-2. file-explorer - if its a file, show its content on code editor
-                    else expand its directory
-3. writing-code - sends the code writing on the web to container and s3
-4. terminal-write - sends command and receives output
-5. close-inbrowser - closes the browser present inside the webapp
-5. opens-inbrowser - opens the browser present inside the webapp
+// 1. newbox - when a new box is creating...if its done, navigates to coding page else shows loading... on screen
+// 2. file-click - if its a file, show its content on code editor
+//                     else expand its directory
+// 3. coding - sends the code writing on the web to container and s3
+// 4. terminal-write - sends command and receives output
+// 5. close-inbrowser - closes the browser present inside the webapp
+// 5. opens-inbrowser - opens the browser present inside the webapp
+
--- src/ws.ts
+++ src/ws.ts
@@ -14,10 +14,31 @@ export function initWs(httpServer:any) {
 function socketHandlers(ws:WebSocket) {
     ws.on('message', (data:any)=>{
         const message = JSON.parse(data);
-        if(message.event === 'new-box') {
-         
-         ws.send('creating new box...');
+        if(message.event === 'newbox') {
+            // copyCodeFromS3()
+            // install dependencies
+            //sends current file structure
         }
-    })
+        else if(message.event === 'file-click') {
+            //fires when clicked on file explorer.
+            // if (file) {
+            //     return fileContent
+            // }
+            // else {    
+            //     return files in the current folder 
+            // }
+        }
+        else if(message.event === 'coding') {
+            // fires when user edits the opened file in editor
+            // add debouncing on fronend
+            // take the data coming from browser
+            // open the corresponding file and update it with this new data
+        }
+        else if(message.event === 'terminal-write') {
+            //take the keystrokes from frontend 
+            //execute the command on server 
+            // send the output to frontend 
+        }
+    });
 }
 
